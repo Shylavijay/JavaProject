5f9e7f51e378ea3c9992085249149d0db4ad2104
shyla <shylavijaykumar27@gmail.com>
2022-11-28 14:24:24 +0530

    Adding Ifelseif

---
+++ student/src/student/IfElse.java
@@ -0,0 +1,21 @@
+package student;
+
+public class IfElse 
+{
+public static void main(String args[])
+{
+	int x=30;
+	if(x<20)
+	{
+		System.out.print("this is if statement");
+	}
+	else
+	{
+		System.out.print("this is else statement");
+	}
+
+}
+}
+
+
+
---
+++ student/src/student/IfElseEx.java
@@ -0,0 +1,20 @@
+package student;
+
+public class IfElseEx {
+public static void main(String args [])
+{
+	int Num=0;
+	if(Num>0)
+	{
+		System.out.println("Positive Number");
+	}
+	else if(Num<0)
+	{
+		System.out.println("Negative Number");
+	}
+	else
+	{
+		System.out.println("Given Number is Zero");
+	}
+	}
+}
