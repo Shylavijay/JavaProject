fcc1292b9041a085b5d3af765010f2cf5596982d
shyla <shylavijaykumar27@gmail.com>
2022-12-01 13:17:48 +0530

    added ifelse example2

---
+++ student/src/student/IfElseEx2.java
@@ -0,0 +1,26 @@
+package student;
+
+public class IfElseEx2
+{
+public static void main(String args[])
+{
+	int X=40;
+	if(X==40)
+	{
+	System.out.print("Value of X is 40");
+		}
+	else if(X==20)
+	{
+	System.out.print("Value of X is 20");
+	}
+	else if(X==10)
+	{
+	System.out.print("Value of X is 10");
+	}
+	else
+	{
+		System.out.print("Value of X is not decleared");
+	}
+}
+
+}
